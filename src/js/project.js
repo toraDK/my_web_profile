118328c51d6d7b054304abf03ddd408b106d2272
Tora Digda Kristiawan <116542241+toraDK@users.noreply.github.com>
2025-10-27 16:07:06 +0700

    Fix link case sensitivity for bookaps in project.js

--- src/js/project.js
+++ src/js/project.js
@@ -26,7 +26,7 @@ const projects = [
         title: "BookShelf App",
         description: `This is the "Bookshelf Apps" website, a platform designed to keep track of books you've read and those you haven't. This project was developed as part of the completion of the Dicoding bootcamp for "Belajar Membuat Front-End web untuk pemula" class. This project was built using : html5, css3, javascript`,
         software: ["html5", "css3", "javascript"],
-        link: ["https://toradk.github.io/Bookshelf-App/"],
+        link: ["https://toradk.github.io/bookshelf-Apps/"],
     },
     {
         img: "laundry",
